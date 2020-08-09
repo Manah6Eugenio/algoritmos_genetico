140f6596ac767ce0accc178e62188db80a29a238
Manah6Eugenio <manassescruz.sg@gmail.com>
2020-08-09 16:25:19 -0300

    iniciando população

--- algoritmos_genetico.py
+++ algoritmos_genetico.py
@@ -58,6 +58,19 @@ class Individuo():
                     self.cromossomo[i] = "1"
         print ("Depois %s " % self.cromossomo)
         return self
+    
+class AlgoritmoGenetico():
+    
+    def __init__(self, tamanho_populacao):
+        self.tamanho_populacao = tamanho_populacao
+        self.populacao = []
+        self.geracao = 0
+        self.melhor_populacao = 0
+        
+    def inicializa_populacao(self, espacos, valores, limite_espacos):
+        for i in range(self.tamanho_populacao):
+            self.populacao.append(Individuo(espacos, valores, limite_espacos))
+        self.melhor_populacao = self.populacao[0]
 
 #função inicial
 if __name__ == '__main__':
@@ -88,29 +101,13 @@ if __name__ == '__main__':
         nomes.append(produto.nome)
     limite = 3
     
-    individuo1 = Individuo(espacos, valores, limite)
+    #populacao inicial
+    tamanho_populacao = 20
+    ag = AlgoritmoGenetico(tamanho_populacao)
+    ag.inicializa_populacao(espacos, valores, limite)
+    for i in range(ag.tamanho_populacao):
+        print("*** Individuo %s ***\n" % i,
+              "Espaços = %s \n" % str(ag.populacao[i].espacos),
+              "Valores = %s \n" % str(ag.populacao[i].valores),
+              "Cromossomo = %s " %  str(ag.populacao[i].cromossomo))
     
-    print("\nIndividuo 1:")
-    for i in range(len(lista_produtos)):
-        if individuo1.cromossomo[i] == '1':
-            print("Nome: %s R$ %s" % (lista_produtos[i].nome, lista_produtos[i].valor))
-            
-    individuo1.avaliacao()
-    print('Nota = %s' % individuo1.nota_avaliacao)
-    print('Espaço usado = %s' % individuo1.espaco_usado)
-    
-    individuo2 = Individuo(espacos, valores, limite)
-
-    print("\nIndividuo 2:")
-    for i in range(len(lista_produtos)):
-        if individuo2.cromossomo[i] == '1':
-            print("Nome: %s R$ %s" % (lista_produtos[i].nome, lista_produtos[i].valor))
-            
-    individuo2.avaliacao()
-    print('Nota = %s' % individuo2.nota_avaliacao)
-    print('Espaço usado = %s' % individuo2.espaco_usado)
-    
-    individuo1.crossover(individuo2)
-    
-    individuo1.mutacao(0.05)
-    individuo2.mutacao(0.05)
